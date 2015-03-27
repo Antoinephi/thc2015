43125b3c006ba0f7c83a9f560e507e4c40c0eb2d
Pierre Falez <pierre.falez@gmail.com>
2015-03-27 21:04:43 +0100

    A accepter

--- src/Main.java
+++ src/Main.java
@@ -7,20 +7,36 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+enum CaseType {
+	H,
+	T
+}
+
+class Coord {
+	public Coord(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	public int x;
+	public int y;
+}
+
+class Part {
+	public int x1;
+	public int x2;
+	public int y1;
+	public int y2;
+}
 
 public class Main {
 	
-	enum CaseType {
-		H,
-		T
-	}
+
 	
-	class Part {
-		int x1;
-		int x2;
-		int y1;
-		int y2;
-	}
+	static int nbLine;
+	static int nbColumn;
+	static int minHPerPart;
+	static int maxCasePerPart;
+	static CaseType[][] pizza;
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader reader = new BufferedReader(new FileReader("data/test_round.in"));
@@ -28,13 +44,13 @@ public class Main {
 		String currentLine = reader.readLine();
 		Scanner sc = new Scanner(currentLine);
 		
-		int nbLine = sc.nextInt();
-		int nbColumn = sc.nextInt();
+		nbLine = sc.nextInt();
+		nbColumn = sc.nextInt();
 		
-		int minHPerPart = sc.nextInt();
-		int maxCasePerPart = sc.nextInt();
+		minHPerPart = sc.nextInt();
+		maxCasePerPart = sc.nextInt();
 		
-		CaseType[][] pizza = new CaseType[nbLine][];
+		pizza = new CaseType[nbLine][];
 		
 		for(int i=0; i<nbLine; i++) {
 			pizza[i] = new CaseType[nbColumn];
@@ -50,6 +66,12 @@ public class Main {
 		 * Algorithm
 		 */
 		
+		Coord c1 = getBestPart(0, 0, 2);
+		if(c1 != null)
+			System.out.println("x="+c1.x+",y="+c1.y);
+		else
+			System.out.println("fuck");
+		
 		/*
 		 * Output
 		 */
@@ -62,12 +84,39 @@ public class Main {
 		}
 		
 		write.close();
+	}
+
+	private static Coord getBestPart(int begX, int begY, int maxDimX, int maxDimY) {
 		
+		while(true) {
+			for(int i=0; i<maxDimX; i++) {
+				for(int j=0; j<maxDimY; j++) {
+					if(partAccepted(begX, begY, i, j)) {
+						return new Coord(begX+i, begY+j);
+					}
+				}
+			}
+			
+			Coord1 = getBestPart(int begX, int begY, int maxDimX, int maxDimY);
+			
+		}
+	}
+
+	private static boolean partAccepted(int x1, int y1, int x2, int y2) {
+		int nbH = 0;
+		for(int i=x1; i<x2; i++) {
+			for(int j=y1; j<y2; j++) {
+				if(pizza[i][j] == CaseType.H) {
+					nbH++;
+				}
+			}
+		}
 		
+		/*
+		 * TODO collision
+		 */
 		
-		
-		
-		
+		return nbH >= minHPerPart && Math.abs(x2-x1)*Math.abs(y2-y1) <= maxCasePerPart;
 	}
 
 }
