85329fff868b46398981f65b4ebf47b60bc2e82a
Pierre Falez <pierre.falez@gmail.com>
2015-03-27 20:44:36 +0100

    Add Output

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 
@@ -9,6 +13,13 @@ public class Main {
 	enum CaseType {
 		H,
 		T
+	}
+	
+	class Part {
+		int x1;
+		int x2;
+		int y1;
+		int y2;
 	}
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
@@ -20,7 +31,7 @@ public class Main {
 		int nbLine = sc.nextInt();
 		int nbColumn = sc.nextInt();
 		
-		int maxHPerPart = sc.nextInt();
+		int minHPerPart = sc.nextInt();
 		int maxCasePerPart = sc.nextInt();
 		
 		CaseType[][] pizza = new CaseType[nbLine][];
@@ -30,9 +41,29 @@ public class Main {
 			currentLine = reader.readLine();
 			for(int j=0; j<nbColumn; j++) {
 				pizza[i][j] = currentLine.charAt(j) == 'H' ? CaseType.H : CaseType.T;
-				System.out.println(pizza[i][j]);
 			}
 		}
+		
+		List<Part> parts = new ArrayList<Part>();
+		
+		/*
+		 * Algorithm
+		 */
+		
+		/*
+		 * Output
+		 */
+		
+		FileWriter write = new FileWriter("data/out");
+		
+		write.write(parts.size()+"\n");
+		for(int i=0; i<parts.size(); i++) {
+			write.write(parts.get(i).x1+" "+parts.get(i).y1+" "+parts.get(i).x2+" "+parts.get(i).y2+"\n");
+		}
+		
+		write.close();
+		
+		
 		
 		
 		
